f53324bd18a55a36d9f4146fee534f461abec05c
maheshnish <maheshnishiii@gmail.com>
2024-08-20 23:28:11 -0600

    adding repo

---
+++ src/main/java/com/example/election_new_system/ElectionNewSystemMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.election_new_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectionNewSystemMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectionNewSystemMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/election_new_system/ElectionNewSystemMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.election_new_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectionNewSystemMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
