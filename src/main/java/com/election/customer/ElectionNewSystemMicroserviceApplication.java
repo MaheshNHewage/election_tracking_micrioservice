b1d674c88c9a1424fbafdc5f38d7f7701785c468
maheshnish <maheshnishiii@gmail.com>
2024-08-20 23:40:10 -0600

    adding new folders

--- src/main/java/com/election/customer/ElectionNewSystemMicroserviceApplication.java
+++ src/main/java/com/election/customer/ElectionNewSystemMicroserviceApplication.java
@@ -1,4 +1,4 @@
-package com.example.election_new_system;
+package com.election.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/election/customer/ElectionNewSystemMicroserviceApplicationTests.java
+++ src/test/java/com/election/customer/ElectionNewSystemMicroserviceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.election_new_system;
+package com.election.customer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
