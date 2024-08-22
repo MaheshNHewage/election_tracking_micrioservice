1bd0f2a384e3706e2720d20f7a94bdbbcf0c3905
Anusha-Hewage <hhewage@gmail.com>
2024-08-22 22:10:51 +1200

    AnuraKD object added

---
+++ src/main/java/com/election/customer/models/AnuraKD.java
@@ -0,0 +1,68 @@
+package com.election.customer.models;
+
+public class AnuraKD {
+    private String nameTest;
+    private String Name;
+    private String possition;
+    private String charateristic;
+    private String goal;
+    private int age;
+
+    public String getNameTest() {
+        return nameTest;
+    }
+
+    public void setNameTest(String nameTest) {
+        this.nameTest = nameTest;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getCharateristic() {
+        return charateristic;
+    }
+
+    public void setCharateristic(String charateristic) {
+        this.charateristic = charateristic;
+    }
+
+    public String getPossition() {
+        return possition;
+    }
+
+    public void setPossition(String possition) {
+        this.possition = possition;
+    }
+
+    public String getGoal() {
+        return goal;
+    }
+
+    public void setGoal(String goal) {
+        this.goal = goal;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+}
+
+
+
+
+
+
+
+
+
