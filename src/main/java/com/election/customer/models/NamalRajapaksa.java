ade1dbb95797a935ab7789849c059c37ea80f359
maheshnish <maheshnishiii@gmail.com>
2024-08-21 23:51:00 -0600

    NameRajapaksha Object added

---
+++ src/main/java/com/election/customer/models/NamalRajapaksa.java
@@ -0,0 +1,58 @@
+package com.election.customer.models;
+
+public class NamalRajapaksa {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String status;
+    private String email;
+    private int votes;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getVotes() {
+        return votes;
+    }
+
+    public void setVotes(int votes) {
+        this.votes = votes;
+    }
+}
