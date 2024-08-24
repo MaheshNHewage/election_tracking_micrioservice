6dbe0d9746cfcc2768f634bbf32823d05c458e36
maheshnish <maheshnishiii@gmail.com>
2024-08-23 23:58:22 -0600

    candidate objects added

---
+++ src/main/java/com/election/customer/models/PoliticalParty.java
@@ -0,0 +1,14 @@
+package com.election.customer.models;
+
+import lombok.Data;
+
+@Data
+public class PoliticalParty {
+
+    private String name;
+    private String color;
+    private String logoName;
+    private String registeredDate;
+
+
+}
