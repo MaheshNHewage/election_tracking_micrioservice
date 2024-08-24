74f24805d67b8f860a3e0c9ecf81a0ee62ebb657
Dilani.Hewage <hdakanchana@gmail.com>
2024-08-24 09:51:38 +0530

    Ranil object added

---
+++ src/main/java/com/election/customer/models/Ranil.java
@@ -0,0 +1,43 @@
+package com.election.customer.models;
+
+public class Ranil{
+        private String name;
+        private String party;
+        private int age;
+        private int politicsYears;
+
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getParty() {
+            return party;
+        }
+
+        public void setParty(String party) {
+            this.party = party;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public int getPoliticsYears() {
+            return politicsYears;
+        }
+
+        public void setPoliticsYears(int politicsYears) {
+            this.politicsYears = politicsYears;
+        }
+
+
+}
